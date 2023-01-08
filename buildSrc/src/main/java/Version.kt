d5b2b757f283448a1cd4349a5ebc8de885723130
Phat Nguyen <phat01283249260@gmail.com>
2023-01-08 23:49:53 +0700

    update manage version and dependency

---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,19 @@
+object Version {
+   const val ANDROIDX_CORE_KTX = "1.7.0"
+   const val ANDROIDX_APPCOMPAT = "1.5.1"
+   const val MATERIAL = "1.7.0"
+   const val ANDROIDX_CONSTRAINTLAYOUT = "2.1.4"
+   const val ANDROIDX_LIFECYCLE = "2.5.1"
+   const val ANDROIDX_NAVIGATION = "2.5.3"
+   const val JUNIT_JUNIT = "4.13.2"
+   const val ANDROIDX_TEST_JUNIT = "1.1.4"
+   const val ANDROIDX_TEST_ESPRESSO_CORE = "3.5.0"
+
+
+}
+
+object SDK {
+   const val MINSDK = 21
+   const val COMPILESDK = 32
+   const val TARGETSDK = 32
+}
